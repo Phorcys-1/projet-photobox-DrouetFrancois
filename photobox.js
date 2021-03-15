911477975d77197b51eb87de3581b5c0821d17f8
Jules <jfrancois968@gmail.com>
2021-03-15 15:51:10 +0100

    fin cours

--- photobox.js
+++ photobox.js
@@ -5,27 +5,8 @@ import {loadPicture} from "./lib/photoloader.js"
 
 document.querySelector('#load_gallery')
   .addEventListener('click',  e => {
-    console.log("click");
-
+    console.log("loading galery....");
     load();
-
-    /*
-    lIdPhotos.forEach(element => {
-      sectionPhoto.innerHTML += `
-      <div class="vignette" >
-      <img data-uri="/www/canals5/photobox/photos/${element}"
-        src="${loadPicture(element)}">
-        <h4 id = "la_categorie"></div>
-        <ul id = "les_commentaires">
-          <li></li>
-          </ul>
-    </div>
-      `;
-      
-    });
-    */
-
-    
   })
 
 
