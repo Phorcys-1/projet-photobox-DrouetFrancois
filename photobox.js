939a587ae966824b6ce33794990d2cac8da8ba2b
Jules <jfrancois968@gmail.com>
2021-03-15 15:34:19 +0100

    ouai

--- photobox.js
+++ photobox.js
@@ -1,11 +1,33 @@
-import {load} from "./lib/gallery.js";
+import {load, lIdPhotos} from "./lib/gallery.js";
+import {loadPicture} from "./lib/photoloader.js"
+
+
 
 document.querySelector('#load_gallery')
   .addEventListener('click',  e => {
+    console.log("click");
 
+    load();
+
+    /*
+    lIdPhotos.forEach(element => {
+      sectionPhoto.innerHTML += `
+      <div class="vignette" >
+      <img data-uri="/www/canals5/photobox/photos/${element}"
+        src="${loadPicture(element)}">
+        <h4 id = "la_categorie"></div>
+        <ul id = "les_commentaires">
+          <li></li>
+          </ul>
+    </div>
+      `;
+      
+    });
+    */
+
+    
   })
 
 
 
-load();
 console.log("coucou");
