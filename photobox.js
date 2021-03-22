bf7b5a658a5fe728fd26b721dda2b343bafbe2ad
Jules <jfrancois968@gmail.com>
2021-03-22 15:59:38 +0100

    on click

--- photobox.js
+++ photobox.js
@@ -1,15 +1,18 @@
 import {load, lIdPhotos} from "./lib/gallery.js";
 import {loadPicture} from "./lib/photoloader.js"
-import {displayPicture} from "./lib/ui.js"
+import {displayPicture, displayGallery} from "./lib/ui.js"
 
 
+//window.addEventListener("load",function () {
+  
 
 document.querySelector('#load_gallery')
   .addEventListener('click',  e => {
     console.log("loading galery....");
-    load();
+    let promesse = load();
+    promesse.then(displayGallery);
   })
-
+//})
 
 
 console.log("coucou");
